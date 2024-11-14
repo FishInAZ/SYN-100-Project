d8e1f9a009ed82b596b4ad7fe63da271682f4599
FishInAZ <103294608+FishInAZ@users.noreply.github.com>
2024-11-14 11:47:29 -0800

    Updated game logic

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/josh/Desktop/SYN 100/venv"
+set -gx VIRTUAL_ENV "/Users/josh/Documents/GitHub/SYN 100/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
